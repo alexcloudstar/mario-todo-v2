5fff4277d101834ae4e28571b4a7ebdc56e3415f
alexcloudstar <alexcloudstar@gmail.com>
2023-06-15 10:59:30 +0300

    fe: Add Super Mario Font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,9 @@ import { Input } from './components/Input';
 const App = () => {
   return (
     <main className='flex flex-col items-center h-[700px] w-[700px] bg-slate-200 overflow-y-auto rounded-[25px] shadow-custom'>
-      <h1 className='text-3xl font-bold text-slate-900 my-6'>Your Todos</h1>
+      <h1 className='text-3xl font-bold text-slate-900 my-6 font-smfont'>
+        Your Todos
+      </h1>
       <Input />
       <Todos />
     </main>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        smfont: ['SMFont', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      },
       boxShadow: {
         custom: '0px 0px 54px -8px rgba(0,0,0,0.75)',
         button: '0px 5px 16px 0px rgba(0,0,0,0.4);',
